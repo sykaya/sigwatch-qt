ffa48d91803d9af3b9ca30117e4b1487c109bb13
sykaya <148535670+sykaya@users.noreply.github.com>
2026-07-31 18:05:56 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication aa(argc, argv);
+
+    // app settings etc
+    aa.setApplicationName("SigWatch");
+    aa.setApplicationDisplayName("sykaya - Spectrum Analyzer");
+    aa.setOrganizationName("sykaya");
+
+    MainWindow www;
+    www.show();
+
+    return aaa.exec();
+}
